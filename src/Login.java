06c885ea4aca982ae820dba0727f61fb6b0fc360
Lilly <liliya.mihaylova@inveitix.com>
2018-03-19 15:08:03 +0200

    remove comment

--- src/Login.java
+++ src/Login.java
@@ -15,7 +15,7 @@ public class Login {
     }
 
     public void createForm() {
-        //// Some comment
+        
         JFrame frame = new JFrame("MyForm");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
